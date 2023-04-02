7e587c0196b0a33d569922d0fd3501d8b559b13f
bulat <bulasad@gmail.com>
2023-04-02 11:35:19 +0300

    fix

--- packages/client/src/pages/Profile/ProfilePage.tsx
+++ packages/client/src/pages/Profile/ProfilePage.tsx
@@ -76,21 +76,17 @@ export const ProfilePage = () => {
               </div>
             </div>
             <div className={s.profilePage__content_right}>
-              <InfoRow name={'Email'} value={user.email}></InfoRow>
-              <InfoRow name={'Login'} value={user.login}></InfoRow>
-              <InfoRow name={'First Name'} value={user.first_name}></InfoRow>
-              <InfoRow name={'Second Name'} value={user.second_name}></InfoRow>
-              <InfoRow name={'Phone'} value={user.phone}></InfoRow>
+              <InfoRow name={'Email'} value={user.email} />
+              <InfoRow name={'Login'} value={user.login} />
+              <InfoRow name={'First Name'} value={user.first_name} />
+              <InfoRow name={'Second Name'} value={user.second_name} />
+              <InfoRow name={'Phone'} value={user.phone} />
             </div>
           </div>
           <div className={s.profilePage__actions}>
-            <Button
-              text={'Edit Profile'}
-              path={PATHNAMES.PROFILE_EDIT}></Button>
-            <Button
-              text={'Change Password'}
-              path={PATHNAMES.PASSWORD_EDIT}></Button>
-            <Button text={'Logout'} onClick={onLogout}></Button>
+            <Button text={'Edit Profile'} path={PATHNAMES.PROFILE_EDIT} />
+            <Button text={'Change Password'} path={PATHNAMES.PASSWORD_EDIT} />
+            <Button text={'Logout'} onClick={onLogout} />
           </div>
         </div>
       )}
