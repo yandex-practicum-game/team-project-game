db661dabbd2fd9da6776ffd2c8d11547f1fe523d
bulat <bulasad@gmail.com>
2023-04-15 16:19:15 +0300

    fix team name and some small style fix

--- packages/client/src/api/config.ts
+++ packages/client/src/api/config.ts
@@ -1,5 +1,5 @@
 export enum API_CONFIG {
   BASE_URL = 'https://ya-praktikum.tech/api/v2',
   RESOURCES_URL = 'https://ya-praktikum.tech/api/v2/resources',
-  TEAM_NAME = 'team_5_quack_attack',
+  TEAM_NAME = 'team5_quack_attack',
 }
