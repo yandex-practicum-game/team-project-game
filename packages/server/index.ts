0414ec18913ed78791db7fb82a7e8debe6bc1440
Johnny-sel <68633595+Johnny-sel@users.noreply.github.com>
2023-04-21 22:56:54 +0300

    configure express server for ssr react

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,18 +1,24 @@
 import dotenv from 'dotenv'
 import cors from 'cors'
+import path from 'path'
+
 dotenv.config()
 
 import express from 'express'
 import { createClientAndConnect } from './db'
 
 const app = express()
-app.use(cors())
+const entryFile = path.resolve('..', 'client', 'dist', 'index.html')
+const staticFolder = path.resolve('..', 'client', 'dist')
 const port = Number(process.env.SERVER_PORT) || 3001
+
+app.use(express.static(staticFolder))
+app.use(cors())
 
 createClientAndConnect()
 
-app.get('/', (_, res) => {
-  res.json('👋 Howdy from the server :)')
+app.get('*', (_, res) => {
+  res.sendFile(entryFile)
 })
 
 app.listen(port, () => {
