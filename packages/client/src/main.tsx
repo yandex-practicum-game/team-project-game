fcd50c0257b045ad0872e978c062d86940a9fedd
Тимур Марокко <69273257+proofblame@users.noreply.github.com>
2023-06-13 09:43:52 +0300

    turn on service worker

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -24,5 +24,5 @@ export const App = () => (
 ReactDOM.hydrateRoot(document.getElementById('root') as HTMLElement, <App />)
 
 if (process.env.NODE_ENV === 'production') {
-  // serviceWorkerRegistration()
+  serviceWorkerRegistration()
 }
