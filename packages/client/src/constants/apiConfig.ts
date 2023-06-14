c91471f4fa43ad30154b7847f8df6add5e975210
bulat <bulasad@gmail.com>
2023-06-14 23:47:46 +0500

    fix docker-compose.yml and dockerFile

--- packages/client/src/constants/apiConfig.ts
+++ packages/client/src/constants/apiConfig.ts
@@ -1,19 +1,19 @@
 export enum API_CONFIG {
   TEAM_NAME = 'team5_quack_attack',
 
-  REDIRECT_URI_PROD = 'http://localhost',
+  REDIRECT_URI_PROD = 'https://thequackattack-galaxiangame-24.ya-praktikum.tech',
   REDIRECT_URI_DEV = 'http://localhost:3000',
 
-  OAUTH_URL_PROD = 'http://localhost/api/v2/oauth',
+  OAUTH_URL_PROD = 'https://thequackattack-galaxiangame-24.ya-praktikum.tech/api/v2/oauth',
   OAUTH_URL_DEV = 'http://localhost:3000/api/v2/oauth',
 
-  BASE_URI_PROD = 'http://localhost/api/v2',
+  BASE_URI_PROD = 'https://thequackattack-galaxiangame-24.ya-praktikum.tech/api/v2',
   BASE_URI_DEV = 'http://localhost:3000/api/v2',
 
-  BASE_URI_PROD_V1 = 'http://localhost/api/v1',
+  BASE_URI_PROD_V1 = 'https://thequackattack-galaxiangame-24.ya-praktikum.tech/api/v1',
   BASE_URI_DEV_V1 = 'http://localhost:3000/api/v1',
 
-  RESOURCES_URL_PROD = 'http://localhost/api/v2/resources',
+  RESOURCES_URL_PROD = 'https://thequackattack-galaxiangame-24.ya-praktikum.tech/api/v2/resources',
   RESOURCES_URL_DEV = 'http://localhost:3000/api/v2/resources',
 }
 
